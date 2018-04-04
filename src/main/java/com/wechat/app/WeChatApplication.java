bb82d89010d46de74d092e8703affb158dca9f6e
jethro <jethrozhu@qq.com>
2018-04-04 16:38:06 +0800

    Initial commit 基本框架搭建完成，数据库设计完成，需求分析完成

---
+++ src/main/java/com/wechat/app/WeChatApplication.java
@@ -0,0 +1,12 @@
+package com.wechat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatApplication.class, args);
+    }
+}
